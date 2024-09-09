f7650a3284774ae95cf15dc71d10df95585d40ba
idingsdaleps <idingsdale@postscript.co.uk>
2024-09-09 13:51:26 +0100

    enhanced logging

--- TicketCreate.js
+++ TicketCreate.js
@@ -22,14 +22,17 @@ try {
                 };     
 
 
-               log.debug ("Search by ID", "Looking for ID " + CUSTOMER_ID)
+               log.audit("Search by ID", "Looking for ID " + CUSTOMER_ID)
                var searchByIdCountBody = https.get({
                     url: ZENDESK_URL + "api/v2/search/count?query=type:user ns_externalid:" + CUSTOMER_ID,
                     headers: zendeskHeaderObj,
                 });
 
+               log.debug("ZD Headers", zendeskHeaderObj)
+               log.debug("ID Count Search", searchByIdCountBody.body)
+
                 var searchByIdCount = JSON.parse(searchByIdCountBody.body).count
-                log.debug("Search by ID",  "ID search returned " + searchByIdCount + " results")
+                log.audit("Search by ID",  "ID search returned " + searchByIdCount + " results")
 
                     if(searchByIdCount>0){
                         var searchByIdBody = https.get({
@@ -37,8 +40,10 @@ try {
                         headers: zendeskHeaderObj,
                     });
 
+                    log.debug("ID Search", searchByIdBody.body)
+
                     var zendeskUserId= JSON.parse(searchByIdBody.body).users[0].id
-                    log.debug ("Search by ID success", "Creating ticket using ID " + zendeskUserId)
+                    log.audit ("Search by ID success", "Creating ticket using ID " + zendeskUserId)
 
                     } else
 
@@ -46,33 +51,35 @@ try {
 
                         if (!CUSTOMER_EMAIL){
 
-                        log.debug("No email", "No email address, creating customer using ID only")
+                        log.audit("No email", "No email address, creating customer using ID only")
                         var user_json = {"user": {"name": CUSTOMER_NAME,"user_fields": {"ns_externalid": CUSTOMER_ID}}}
                         var createUserResponse = https.post({
                         url: ZENDESK_URL + "api/v2/users/create_or_update",
                         headers: zendeskHeaderObj,
                         body: JSON.stringify(user_json)
                         });
+                        log.debug("User Create Response", createUserResponse.body)
                         if (createUserResponse.code==201){
                         var zendeskUserId = JSON.parse(createUserResponse.body).user.id
-                        log.debug("User Created", "User ID " + zendeskUserId+ " Created")
+                        log.audit("User Created", "User ID " + zendeskUserId+ " Created")
                         } 
                             else{
-                            log.debug("User Creation Failed", createUserResponse)
-                            log.debug("User JSON", user_json)
+                            log.audit("User Creation Failed", createUserResponse)
+                            log.audit("User JSON", user_json)
                             }
 
                         }
 
                         else{
 
-                        log.debug ("Search by ID failed", "No results from ID search, looking for email " + CUSTOMER_EMAIL)
+                        log.audit ("Search by ID failed", "No results from ID search, looking for email " + CUSTOMER_EMAIL)
                         var searchByEmailCountBody = https.get({
                         url: ZENDESK_URL + "api/v2/search/count?query=type:user " + CUSTOMER_EMAIL,
                         headers: zendeskHeaderObj,
                         });
                         var searchByEmailCount = JSON.parse(searchByEmailCountBody.body).count
-                        log.debug("Search by email",  "Email search returned " + searchByEmailCount + " results")
+                        log.audit("Search by email",  "Email search returned " + searchByEmailCount + " results")
+                        log.debug("Email Count Search", searchByEmailCount.body)
 
                         if(searchByEmailCount>0){
 
@@ -82,24 +89,26 @@ try {
                         });
 
                         var zendeskUserId= JSON.parse(searchByEmailBody.body).users[0].id
-                        log.debug("Search by email success", "Creating ticket using ID " + zendeskUserId)
+                        log.audit("Search by email success", "Creating ticket using ID " + zendeskUserId)
+                        log.debug("Email Search", searchByEmailBody.body)
 
                             } 
                             else {
-                            log.debug("No results", "No results found for ID or Email, creating new user")
+                            log.audit("No results", "No results found for ID or Email, creating new user")
                             var user_json = {"user": {"name": CUSTOMER_NAME,"email": CUSTOMER_EMAIL,"user_fields": {"ns_externalid": CUSTOMER_ID}}}
                             var createUserResponse = https.post({
                             url: ZENDESK_URL + "api/v2/users/create_or_update",
                             headers: zendeskHeaderObj,
                             body: JSON.stringify(user_json)
                             });
+                            log.debug("User Creation", createUserResponse.body)
                             if (createUserResponse.code==201){
                             var zendeskUserId = JSON.parse(createUserResponse.body).user.id
-                            log.debug("User Created", "User ID " + createdUserId + " Created")
+                            log.audit("User Created", "User ID " + createdUserId + " Created")
                             } 
                                 else{
-                                log.debug("User Creation Failed", createUserResponse)
-                                log.debug("User JSON", user_json)
+                                log.audit("User Creation Failed", createUserResponse)
+                                log.audit("User JSON", user_json)
                                 }
                             }
 
@@ -112,14 +121,15 @@ try {
             headers: zendeskHeaderObj,
             body: JSON.stringify(ticket_json)
             });
+            log.debug("Ticket Creation", createTicketResponse.body)
             if (createTicketResponse.code==201){
             var createdTicketId = JSON.parse(createTicketResponse.body).ticket.id
-            log.debug("Ticket Created", "Ticket ID " + createdTicketId + " Created")
+            log.audit("Ticket Created", "Ticket ID " + createdTicketId + " Created")
             return createdTicketId
             } 
             else{
-            log.debug("Ticket Creation Failed", createTicketResponse)
-            log.debug("Ticket JSON", ticket_json)
+            log.audit("Ticket Creation Failed", createTicketResponse)
+            log.audit("Ticket JSON", ticket_json)
             }
 
 
@@ -128,7 +138,7 @@ try {
  }
      
         catch (e) {
-            log.debug("ERROR : " + e.name, e.message);
+            log.audit("ERROR : " + e.name, e.message);
         }
     }
 
