21b0bea7b4ef14e520986a6034629fbff541ec09
idingsdaleps <idingsdale@postscript.co.uk>
2024-11-14 11:33:18 +0000

    added restock alerts

--- nbs272_salesorder_cs.js
+++ nbs272_salesorder_cs.js
@@ -7,9 +7,9 @@
  * 2.0          12 June 2019        NoBlue(BO)      added  CH-78 -> count inventory items
  * 2.1          18 Sept 2020        NoBlue(SDK)     Adding Sublist count
  */
-define(['N/record', 'N/runtime', 'N/search', 'N/log'],
+define(['N/record', 'N/runtime', 'N/search', 'N/log', 'N/ui/dialog', 'N/ui/message'],
 
-function(record, runtime, search, log) {
+function(record, runtime, search, log, dialog, message) {
     
     /**
      * Function to be executed after page is initialized.
@@ -38,14 +38,14 @@ function(record, runtime, search, log) {
                 
                 var entityId = newRecord.getValue({fieldId:'entity'});
 
-
-
                 
 
                 if (entityId){
                     log.debug('entityId',entityId);
                     itemsPurchased = getPreviousItems(entityId);
-                    console.log(itemsPurchased)
+                    console.log(itemsPurchased);
+
+                    
                     var entityFields = [];
                     for(var f in fields){
                         entityFields.push(fields[f]);       
@@ -107,6 +107,21 @@ function(record, runtime, search, log) {
     
     function fieldChanged(scriptContext) {
 
+
+        if (scriptContext.fieldId=='leadsource'){
+
+            var currentRecord = scriptContext.currentRecord;
+            var entityId = currentRecord.getValue({fieldId:'entity'});
+            if(!!entityId){
+
+            itemsLost = getLostItems(entityId)
+            console.log(itemsLost);
+            if (itemsLost.length>0){
+                lostPopup(itemsLost, scriptContext)
+            }
+        }
+        }
+  
    if (scriptContext.fieldId=='entity'){
 
             var currentRecord = scriptContext.currentRecord;
@@ -116,7 +131,10 @@ function(record, runtime, search, log) {
 
             if (entityId){
                     log.debug('entityId',entityId);
+                    
                     itemsPurchased = getPreviousItems(entityId);
+                    itemsLost = getLostItems(entityId);
+
 
                 }
         }
@@ -158,8 +176,10 @@ function(record, runtime, search, log) {
      * @since 2015.2
      */
     function postSourcing(scriptContext) {
+
+
         if (runtime.executionContext != runtime.ContextType.WEBSTORE){
-            
+
             if (scriptContext.fieldId === 'entity'){
                 setSalesOrderFieldsFromCustomer(scriptContext.currentRecord);
             }
@@ -399,7 +419,9 @@ function(record, runtime, search, log) {
               "AND", 
               ["taxline","is","F"], 
               "AND", 
-              ["shipping","is","F"]
+              ["shipping","is","F"], 
+              "AND",
+              ["sum(quantity)","greaterthan","0"]
            ]
 
         var salesorderSearchObj = search.create({
@@ -414,6 +436,179 @@ function(record, runtime, search, log) {
         });
         return itemsPurchased;
     }
+
+
+
+    function getLostItems(customerId){
+        var itemsLost = [];
+
+
+        var columns = [
+             search.createColumn({
+             name: "itemid",
+             join: "item",
+             summary: "GROUP",
+             label: "Name"
+          }),
+             search.createColumn({
+             name: "internalid",
+             join: "item",
+             summary: "GROUP",
+             label: "Internal ID"
+          }),
+          search.createColumn({
+             name: "displayname",
+             join: "item",
+             summary: "GROUP",
+             label: "Display Name"
+          }),
+          search.createColumn({
+             name: "quantity",
+             summary: "SUM",
+             label: "Quantity"
+          }),
+          search.createColumn({
+             name: "custcol_nbs_quantitylost",
+             summary: "COUNT",
+             label: "Quantity Lost"
+          }),
+          search.createColumn({
+             name: "trandate",
+             summary: "MAX",
+             label: "Date"
+          }),
+          search.createColumn({
+             name: "quantityavailable",
+             join: "item",
+             summary: "MAX",
+             label: "Available"
+          })];
+
+        var filters = [      
+            ["type","anyof","SalesOrd"], 
+              "AND", 
+              ["mainline","is","F"], 
+              "AND", 
+              ["taxline","is","F"], 
+              "AND", 
+              ["shipping","is","F"], 
+              "AND", 
+              ["trandate","within","previousoneyear"], 
+              "AND", 
+              ["customer.internalid","anyof",customerId], 
+              "AND", 
+              ["item.quantityavailable","greaterthan","0"], 
+              "AND", 
+              ["sum(quantity)","equalto","0"], 
+              "AND", 
+              ["sum(custcol_nbs_quantitylost)","greaterthan","1"], 
+              "AND", 
+              ["sum(custcol_ps_restock_rejected)","notgreaterthan","0"]
+                   ];
+
+        var salesorderSearchObj = search.create({
+           type: "salesorder",
+           filters: filters,
+           columns: columns
+          });
+
+        var searchResultCount = salesorderSearchObj.runPaged().count;
+        console.log(searchResultCount)
+        
+        salesorderSearchObj.run().each(function(result){
+            itemsLost.push({itemId:result.getValue(columns[0]), name:result.getValue(columns[2]), date:result.getValue(columns[5]), internalId:result.getValue(columns[1])});
+            return true;
+        });
+        return itemsLost;
+    }
+
+
+    function lostPopup(itemsLost, scriptContext){
+
+            var popupMessage = 'Customer has previously wished to buy the following but item(s) were out of stock, do you want to add to the order?<br><br><table><tr><th><b>SKU</b></th><th><b>Title</b></th><th><b>Date</b></th></tr>'
+
+            for (var i = 0; i < itemsLost.length; i++){
+                popupMessage = popupMessage + '<tr><td>' + itemsLost[i].itemId + '</td><td>' + itemsLost[i].name + '</td><td>' + itemsLost[i].date +'</td></tr>'
+            }
+
+            popupMessage = popupMessage + '</table>'
+
+            var options = {
+                title: 'Restock Alert',
+                message: popupMessage,
+                buttons: [
+                    { label: 'Yes', value: 1 },
+                    { label: 'No', value: 2 }
+                ]
+            };
+        
+        
+        function success(result) { 
+            if (result == 1) {
+                acceptRestock(itemsLost, scriptContext);
+            } else if (result == 2) {
+                rejectRestock(itemsLost, scriptContext);
+            } 
+        }
+        function failure(reason) { console.log('Failure: ' + reason) }
+        dialog.create(options).then(success).catch(failure);
+
+    }
+
+    function acceptRestock(restockItems, scriptContext){
+        var currentRecord = scriptContext.currentRecord;
+        for (var i = 0; i < restockItems.length; i++){
+            console.log ('Adding item ' + restockItems[i].internalId + ' to line ' + i)
+            currentRecord.insertLine({ sublistId: 'item', line: i });
+            currentRecord.setCurrentSublistValue({
+                sublistId: 'item',
+                fieldId: 'item',
+                value: restockItems[i].internalId
+            });
+            currentRecord.setCurrentSublistValue({
+                sublistId: 'item',
+                fieldId: 'quantity',
+                value: 1
+            });
+            currentRecord.setCurrentSublistValue({
+                sublistId: 'item',
+                fieldId: 'custcol_ps_restock_accepted',
+                value: 1
+            });
+            currentRecord.commitLine({sublistId:'item'})
+        }
+
+
+    }
+
+    function rejectRestock(restockItems, scriptContext){
+
+
+        var currentRecord = scriptContext.currentRecord;
+        for (var i = 0; i < restockItems.length; i++){
+            console.log ('Adding item ' + restockItems[i].internalId + ' to line ' + i)
+            currentRecord.insertLine({ sublistId: 'item', line: i });
+            currentRecord.setCurrentSublistValue({
+                sublistId: 'item',
+                fieldId: 'item',
+                value: restockItems[i].internalId
+            });
+            currentRecord.setCurrentSublistValue({
+                sublistId: 'item',
+                fieldId: 'quantity',
+                value: 0
+            });
+            currentRecord.setCurrentSublistValue({
+                sublistId: 'item',
+                fieldId: 'custcol_ps_restock_rejected',
+                value: 1
+            });
+            currentRecord.commitLine({sublistId:'item'})
+        }
+
+
+    }
+
 
     /**
      * Validation function to be executed when sublist line is inserted.
@@ -470,7 +665,8 @@ function(record, runtime, search, log) {
         fieldChanged:fieldChanged,
         validateLine:validateLine,
         sublistChanged: sublistChanged,
-        postSourcing: postSourcing
+        postSourcing: postSourcing,
+        lostPopup: lostPopup
     };
     
 });
